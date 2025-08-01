94da5de7e29c6b3d6c8e3e1b9ade711a8d38d9a3
Sajan Nikam <sajannikam111@gmail.com>
2025-08-01 23:20:55 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,10 @@
 resource "aws_s3_bucket" "my-s3-bucket" {
-  bucket_prefix = var.bucket_prefix
-  acl = var.acl
-  
-   versioning {
-    enabled = var.versioning
+  ...
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.my-s3-bucket.id
+  versioning_configuration {
+    status = "Enabled"
   }
-  
-  tags = var.tags
 }
