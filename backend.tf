561794f97c76394a190967de5d2b13256775f7e0
Sajan Nikam <sajannikam111@gmail.com>
2025-08-02 12:50:51 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "my-dev-terraform-project-tdi"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "Terraform-dynamodb-table"
   }
 }
