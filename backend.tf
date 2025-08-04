2f1b51bce6e2370e71ef6758decb8614ca97089e
Chandan Patil CP <98092879+chandancp990@users.noreply.github.com>
2025-08-04 10:35:52 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-prod-terraform-project-tdi"
+    bucket = "my-jenkin-project"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "Automation-dynamodb-table"
